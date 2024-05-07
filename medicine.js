88f0e3386533a93a674d9d0534a2d717265e4949
Md. Anisur Rahman <47313052+anisurrahmanlikhon@users.noreply.github.com>
2024-05-07 23:53:45 +0600

    Update medicine.js

--- medicine.js
+++ medicine.js
@@ -61,7 +61,7 @@
             });
 
             if (results.length === 0) {
-                medicineResultsElement.innerHTML = "<p>No medicine is found. Please select or enter only symptom.</p>";
+                medicineResultsElement.innerHTML = "<p>No medicines found. Select/Enter only one symptom.</p>";
             } else {
                 let resultHTML = "<h3>Medicines for Selected Symptom</h3><ul>";
                 results.forEach(medicine => {
